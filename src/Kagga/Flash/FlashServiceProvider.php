deca772e27445dee8eaa5b506a8f4c9ebf9a9354
johnkagga <johnkagga@gmail.com>
2016-02-16 17:07:32 +0300

    Corrected view path

--- src/Kagga/Flash/FlashServiceProvider.php
+++ src/Kagga/Flash/FlashServiceProvider.php
@@ -18,10 +18,10 @@ class FlashServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->loadViewsFrom(__DIR__ . '/../../views', 'flash');
+        $this->loadViewsFrom(__DIR__ . '/../../resources/views', 'flash');
 
         $this->publishes([
-            __DIR__ . '/../../views' => base_path('resources/views/vendor/flash')
+            __DIR__ . '/../../resources/views' => base_path('resources/views/vendor/flash')
         ]);
     }
 
